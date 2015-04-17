0dd232ec8ba7f7d94f6fcca8f0cb1c59444b2c15
yugan2005X230Win <xiongzhehong@gmail.com>
2015-04-17 10:08:08 -0700

    DungeonGame Solved!

--- Leetcode/src/solutions/java/DungeonGame.java
+++ Leetcode/src/solutions/java/DungeonGame.java
@@ -4,56 +4,46 @@ public class DungeonGame {
 	
     public int calculateMinimumHP(int[][] dungeon) {
         if(dungeon==null||dungeon.length==0||dungeon[0].length==0) return 1;
-        int[][] mostNegative = new int[dungeon.length][dungeon[0].length];
-        int[][] sumOfPath = new int[dungeon.length][dungeon[0].length];
-        mostNegative[0][0]=dungeon[0][0];
-        sumOfPath[0][0]=dungeon[0][0];
+        int rowNum = dungeon.length;
+        int colNum = dungeon[0].length;
+        int[][] req = new int[rowNum][colNum];
+        int reqMinForPrev=0;
+        /*
+         * The logic is like this:
+         * Look from the princess towards to the hero. The number at each grid need satisfy two requirement
+         * 1: The number must make the current gird positive
+         * 2: After the operation of the current gird, it must meet the previous grid's requirement.
+         * So, instead the original wrong way of finding the path, we just find the required numbers for each grid
+         * in the reverse order.
+         */
+        req[rowNum-1][colNum-1]= (dungeon[rowNum-1][colNum-1]>0)?1:(-1*dungeon[rowNum-1][colNum-1]+1);
         
-        for (int idx=1; idx<dungeon.length; idx++){
-            sumOfPath[idx][0]=sumOfPath[idx-1][0]+dungeon[idx][0];
-            mostNegative[idx][0]=(sumOfPath[idx][0]<mostNegative[idx-1][0])?sumOfPath[idx][0]:mostNegative[idx-1][0];
+        for (int iRow=rowNum-2; iRow>=0; iRow--){
+        	req[iRow][colNum-1]=(dungeon[iRow][colNum-1]>0)?1:(-1*dungeon[iRow][colNum-1]+1);
+        	if (req[iRow][colNum-1]<req[iRow+1][colNum-1]-dungeon[iRow][colNum-1]) {
+        		// this is equivalent to req[iRow][colNum-1]+dungeon[iRow][colNum-1]<req[iRow+1][colNum-1]
+        		req[iRow][colNum-1]=req[iRow+1][colNum-1]-dungeon[iRow][colNum-1];
+        	}
+        }
+        for (int iCol=colNum-2; iCol>=0; iCol--){
+        	req[rowNum-1][iCol]=(dungeon[rowNum-1][iCol]>0)?1:(-1*dungeon[rowNum-1][iCol]+1);
+        	if (req[rowNum-1][iCol]<req[rowNum-1][iCol+1]-dungeon[rowNum-1][iCol]) {
+        		req[rowNum-1][iCol]=req[rowNum-1][iCol+1]-dungeon[rowNum-1][iCol];
+        	}
         }
         
-        for (int idx=1; idx<dungeon[0].length; idx++){
-            sumOfPath[0][idx]=sumOfPath[0][idx-1]+dungeon[0][idx];
-            mostNegative[0][idx]=(sumOfPath[0][idx]<mostNegative[0][idx-1])?sumOfPath[0][idx]:mostNegative[0][idx-1];
+        for (int iRow=rowNum-2; iRow>=0; iRow--){
+        	for (int iCol=colNum-2; iCol>=0; iCol--){
+        		req[iRow][iCol]=(dungeon[iRow][iCol]>0)?1:(-1*dungeon[iRow][iCol]+1);
+        		reqMinForPrev = req[iRow][iCol+1]-dungeon[iRow][iCol];
+        		if (reqMinForPrev>req[iRow+1][iCol]-dungeon[iRow][iCol]) {
+        			reqMinForPrev=req[iRow+1][iCol]-dungeon[iRow][iCol];
+        		}
+        		if (req[iRow][iCol]<reqMinForPrev) req[iRow][iCol]=reqMinForPrev;
+        	}
         }
         
-        for (int idxRow=1; idxRow<dungeon.length; idxRow++){
-            for (int idxCol=1; idxCol<dungeon[0].length; idxCol++){
-                if (mostNegative[idxRow-1][idxCol]<mostNegative[idxRow][idxCol-1]) { //bug #1 here: need use mostNegative to choose the path, not sumOfPath!
-                    sumOfPath[idxRow][idxCol]=sumOfPath[idxRow][idxCol-1]+dungeon[idxRow][idxCol];
-                    mostNegative[idxRow][idxCol]=(sumOfPath[idxRow][idxCol]<mostNegative[idxRow][idxCol-1])?sumOfPath[idxRow][idxCol]:mostNegative[idxRow][idxCol-1];
-
-                }
-                else if (mostNegative[idxRow-1][idxCol]>mostNegative[idxRow][idxCol-1]) {
-                    sumOfPath[idxRow][idxCol]=sumOfPath[idxRow-1][idxCol]+dungeon[idxRow][idxCol];
-                    mostNegative[idxRow][idxCol]=(sumOfPath[idxRow][idxCol]<mostNegative[idxRow-1][idxCol])?sumOfPath[idxRow][idxCol]:mostNegative[idxRow-1][idxCol];
-                }
-                else { // bug #3 here: This bug makes the problem hard - Only use mostNegative to choose the path is not enough. case: [[0,0,0],[1,1,-1]]
-                	// when mostNegative is the same, need use sumOfPath to choose path
-                	// UPDATE: bug #3 fixed, but still does NOT work. Actually this is not a good DP problem. Because the previous subproblem will change
-                	// with the change of the later element. NEED GIVE UP THIS METHOD AND RETHINK
-                	// It looks so much like DP problem which makes it very hard.!
-                	if (sumOfPath[idxRow-1][idxCol]<sumOfPath[idxRow][idxCol-1]) {
-                		sumOfPath[idxRow][idxCol]=sumOfPath[idxRow][idxCol-1]+dungeon[idxRow][idxCol];
-                	}
-                	else {
-                		sumOfPath[idxRow][idxCol]=sumOfPath[idxRow-1][idxCol]+dungeon[idxRow][idxCol];
-                	}
-                	mostNegative[idxRow][idxCol]=(sumOfPath[idxRow][idxCol]<mostNegative[idxRow-1][idxCol])?sumOfPath[idxRow][idxCol]:mostNegative[idxRow-1][idxCol];                	
-                }
-                
-            }
-
-        }
-        
-        int result = mostNegative[dungeon.length-1][dungeon[0].length-1];
-        if (result<0) result = (-1)*result+1;
-        else result =1; // bug #2 here: if it is positive, 1 will be okay.
-        
-        return result; 
-
+        return req[0][0]; 
     }
 
 	public static void main(String[] args) {

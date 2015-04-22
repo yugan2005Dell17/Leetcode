726b962d2bad7aedd1ce0123775213c6d5e98684
yugan2005X230Win <xiongzhehong@gmail.com>
2015-04-22 12:59:26 -0700

    Added a root constructor for UndirectedGraph

--- Leetcode/src/solutions/java/UndirectedGraph.java
+++ Leetcode/src/solutions/java/UndirectedGraph.java
@@ -32,19 +32,52 @@ public class UndirectedGraph {
 		size = counter;
 	}
 	
+	public UndirectedGraph(UndirectedGraphNode root){
+		/*
+		 * This constructor is not totally finished
+		 * 1: the nodes array only holds 10 elements, should use variable length array setting
+		 * 2: If the root is not the real root, the Graph will miss nodes!
+		 * 3: But it is good for my debug usage for now.
+		 */
+		int counter = 0;
+		graphNodes.put(root.label, root);
+		nodes = new UndirectedGraphNode[10];
+		nodes[0]=root;
+		counter++;
+		for (UndirectedGraphNode subNode:root.neighbors){
+			counter = dfsAddNode(subNode, counter);
+		}
+		size = counter;		
+	}
+	
+	private int dfsAddNode(UndirectedGraphNode curNode, int counter){
+		if (!graphNodes.containsKey(curNode.label)){
+			graphNodes.put(curNode.label, curNode);
+			nodes[counter]=curNode;
+			counter++;
+			for (UndirectedGraphNode subNode:curNode.neighbors){
+				counter = dfsAddNode(subNode, counter);
+			}
+		}
+		return counter;
+	}
+
+	
 	public int size(){
 		return size;
 	}
-	//{0,1,2#1,2#2,2}
 	
 	public String toString(){
 		String output="{";
 		for (UndirectedGraphNode curNode:nodes){
-			output+=curNode.label;
-			for (UndirectedGraphNode subNode:curNode.neighbors){
-				output += ","+subNode.label;
+			if (curNode!=null){
+				output+=curNode.label;
+				for (UndirectedGraphNode subNode:curNode.neighbors){
+					output += ","+subNode.label;
+				}
+				output+="#";
 			}
-			output+="#";
+
 		}
 		output=output.substring(0, output.length()-1)+"}";
 		return output;
@@ -52,10 +85,13 @@ public class UndirectedGraph {
 	
 
 	public static void main(String[] args) {
-//		String input = "0,1,5#1,2,5#2,3#3,4,4#4,5,5#5";
-		String input = "0,1,2#1,2#2,2";
+		String input = "0,1,5#1,2,5#2,3#3,4,4#4,5,5#5";
+//		String input = "0,1,2#1,2#2,2";
 		UndirectedGraph test = new UndirectedGraph(input);
 		System.out.println(test);
+		
+		UndirectedGraph test2 = new UndirectedGraph(test.nodes[1]);
+		System.out.println(test2);
 	}
 
 }
